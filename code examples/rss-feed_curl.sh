c46fcefdbc5ccd4cf93494637da6d52913caf114
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 13:34:56 +0200

    Adding code examples/rss-feed_curl.sh

---
+++ code examples/rss-feed_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://news.yahoo.com/rss/politics"}'
